0dd56c07eb80b98ee65b1bf6db7c0bba088fe6e5
PlayErsGames666 <playersgames.ru@gmail.com>
2026-03-21 17:59:15 +0500

    Update structure.

--- lesson6/stroke.py
+++ lesson6/stroke.py
@@ -1,25 +1,27 @@
 from os.path import split
 
 word = 'lovely,hobby'
-#
-# print(len(word))
-#
-# print(word.upper()) # Превращает весь текст в верхний регистр
-# print(word.isupper()) # Boolean, проверяет весь текст на наличие верхнего регистра True/False
-#
-# print(word.lower()) # Превращает весь текст в нижний регистр
-# print(word.islower()) # Boolean, проверяет весь текст на наличие нижнего регистра True/False
-#
-# print(word.capitalize()) # Делает первую букву в верхнем регистре, а всё остальное в нижнем
-#
-# print(word.find('b')) # Ищет символы по заданному значению и выводит их индекс
+
+print(len(word))
+
+print(word.upper()) # Превращает весь текст в верхний регистр
+print(word.isupper()) # Boolean, проверяет весь текст на наличие верхнего регистра True/False
+
+print(word.lower()) # Превращает весь текст в нижний регистр
+print(word.islower()) # Boolean, проверяет весь текст на наличие нижнего регистра True/False
+
+print(word.capitalize()) # Делает первую букву в верхнем регистре, а всё остальное в нижнем
+
+print(word.find('b')) # Ищет символы по заданному значению и выводит их индекс
 
 hobby = word.split(',') # Создаёт список из заданных параметров и разделяет их по символу в скобках
+
+# Цикл делающий из обычного два слова через запятую, списком.
 
 for i in range(len(hobby)):
     print(i, hobby[i], hobby)
     hobby[i] = hobby[i].capitalize()
 
-space = ', '.join(hobby)
-print(space)
+comma = ', '.join(hobby)
+print(comma)
 print(hobby)
