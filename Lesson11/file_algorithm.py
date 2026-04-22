db5c240bd88fd3190642f5930ddaadb22fbe42f5
PlayErsGames666 <playersgames.ru@gmail.com>
2026-04-22 15:28:12 +0500

    Update atribute, encoding

--- Lesson11/file_algorithm.py
+++ Lesson11/file_algorithm.py
@@ -1,8 +1,8 @@
 data = input("Enter the data: ") # Создаём переменную с доступом к записи через терминал
 
 # file = open("data/text2.txt", "w") # Создаём и открываем файл text2.txt
-file = open("data/text2.txt", "a") # работает с английскими символами
-
+# file = open("data/text2.txt", "a") # работает с английскими символами
+file = open("data/text2.txt", "a", encoding="utf-8")  # С дополнительным атрибутом encoding, проблем с русскими символами не будет
 file.write(data + '\n') # Записываем в файл данные которые пользователь введёт
 
 file.close() # Закрываем файл
