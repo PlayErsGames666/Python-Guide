f4de9b95ae3ed583f764f89625a24d0c823905b5
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-03 21:03:44 +0500

    Module math squere.

--- Lesson14/modules.py
+++ Lesson14/modules.py
@@ -13,14 +13,16 @@
 # print(d.datetime.now().time().minute) # Выводит текущее время в минутых
 # print(d.datetime.now().date().month) # Выводит текущую дату в месяцах
 
-import sys, os, platform
-import random
-import math
-import array
+# import sys, os, platform
+# import random
+# import array
+#
+# # Все модули представлены в интернете
+#
+# print(sys.path) # Выводит полный путь к текущему проекту
+# print(os.name) # Выводит название операционной системы nt(Windows New Technology), posix(MacOS)
+# print(platform.system()) # Выводит название платформы системы
 
-# Все модули представлены в интернете
+from math import sqrt as s # from откуда импортирую и import что импортирую
 
-print(sys.path) # Выводит полный путь к текущему проекту
-print(os.name) # Выводит название операционной системы nt(Windows New Technology), posix(MacOS)
-print(platform.system()) # Выводит название платформы системы
-
+print(s(100)) # Квадратный корень из 100
