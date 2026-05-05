11865ea88176ca288e03be944c35104d0bc007a9
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-05 19:48:59 +0500

    Update structure

--- Lesson14/usemodulehost.py
+++ Lesson14/usemodulehost.py
@@ -1,4 +1,8 @@
-import modulehost as mhost # Используем созданный модуль как mhost
+# import modulehost as mhost # Используем созданный модуль как mhost
+#
+# print(mhost.name) # выводим mhost и вводим имя
+# mhost.hello() # автоматически выводит hello и имя
 
-print(mhost.name) # выводим mhost и вводим имя
-mhost.hello() # автоматически выводит hello и имя
+from modulehost import add_three_numbers as atn # импортируем ту функцию с сложением
+
+print(atn(10, 20, 30)) # задаём параметры для нашей функции
