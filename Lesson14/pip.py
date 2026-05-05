034e2178a83caf83a893f95abbe9a73749d556f8
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-05 19:55:45 +0500

    COWSAY

---
+++ Lesson14/pip.py
@@ -0,0 +1,6 @@
+# Устанавливаем модель cowsay
+# В терминале прописываем pip install cowsay или зайдите на страничку в pypi.org и скачайте сами
+
+import cowsay
+
+cowsay.cow("HEHEHEH") # Выводит в терминал |
