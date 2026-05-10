40549b9f5e77558f340c36d79cdd6c11e71d0f5b
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-10 20:43:05 +0500

    Optimize attributes by defaulting None

--- Lesson15-16/definclass.py
+++ Lesson15-16/definclass.py
@@ -5,7 +5,7 @@ class Dog: # Открываем класс в который будем впис
     isHappy = None # Счастлив ли
     colour = None # Цвет
 
-    def __init__(self, name, age, sex, isHappy, colour): # Создаём конструктор __init__
+    def __init__(self, name = None, age = None, sex = None, isHappy = None, colour = None): # Создаём конструктор __init__
         # self.name = name  # Задаём ему параметры которые есть в class Dog
         # self.age = age
         # self.sex = sex
@@ -14,7 +14,8 @@ class Dog: # Открываем класс в который будем впис
         self.set_data(name, age, sex, isHappy, colour) # Устанавливает значение, теперь не нужен весь этот верхний код
         self.get_data() # Обращение к методу вывода в терминал
 
-    def set_data(self, name, age, sex, isHappy, colour): # Создаём метод в class Dog
+    def set_data(self, name = None, age = None, sex = None, isHappy = None, colour = None): # Создаём метод в class Dog
+        # = None теперь базовой значение, если его никто не задаст будет None
         self.name = name # Задаём ему параметры которые есть в class Dog
         self.age = age
         self.sex = sex
@@ -30,7 +31,9 @@ class Dog: # Открываем класс в который будем впис
               "Colour:", self.colour, ".")
 
 dog1 = Dog("Dusya", 3, "Male", True, "Grey-Black") # Передача данных сразу же в объект
+dog1 = Dog() # Теперь и в конструкторе есть =None и не обязательно передавать параметры чтобы код заработал
 # dog1.set_data("Dusya", 3, "Male", True, "Grey-Black") # Устанавливаем значения для первой собаки
+dog1.set_data("Dusya", 3, "Male", True) # Теперь можно выводить хоть 0  хоть все параметры, проблем не будет
 
 dog2 = Dog("Jora", 1, "Male", True, "Drak-Blue")
 # dog2.set_data("Jora", 1, "Male", True, "Drak-Blue") # Устанавливаем значения для второй собаки
