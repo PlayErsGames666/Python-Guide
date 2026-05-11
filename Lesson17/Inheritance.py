1ef475627a1f0973d7a78dd0cf5d62cf91b7ec3b
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-11 17:58:42 +0500

    First class architecture.

--- Lesson17/Inheritance.py
+++ Lesson17/Inheritance.py
@@ -1,1 +1,13 @@
 # Inheritance то-есть наследование, В прошлом уроке использовали для class Dog
+class Building:
+    year = None
+    city = None
+
+    def __init__(self, year, city):
+        self.year = year
+        self.city = city
+
+    def get_info(self, year, city):
+        print("Year: ", self.year, "City: ", self.city)
+
+
