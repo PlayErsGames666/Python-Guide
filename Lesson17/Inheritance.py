36561466d74ed3fb72299181084f9408cbc4f22e
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-11 18:16:01 +0500

    Inheritance and bag fix

--- Lesson17/Inheritance.py
+++ Lesson17/Inheritance.py
@@ -1,13 +1,19 @@
 # Inheritance то-есть наследование, В прошлом уроке использовали для class Dog
-class Building:
-    year = None
+class Building: # Создаём класс Building
+    year = None # Задаём параметры год и город
     city = None
 
-    def __init__(self, year, city):
+    def __init__(self, year, city): # Создаём конструктор который будет запускать данные
         self.year = year
         self.city = city
 
-    def get_info(self, year, city):
-        print("Year: ", self.year, "City: ", self.city)
+    def get_info(self): # Создаём метод для вывода данных
+        print("Year: ", self.year, ", City: ", self.city)
 
+class School(Building): # Создаём унаслдованный класс School
+    pass
 
+school = School(2020, "Tashkent") # Вписываем данные
+school.get_info() # Выводим данные
+house = Building(2010, "Tokyo")
+shop = Building(2026, "Samara")
