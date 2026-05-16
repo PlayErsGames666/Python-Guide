f10388a69468fd03dacf15f3965cf950a6d39c60
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-16 12:12:55 +0500

    create webbrowser opener

---
+++ Lesson18/Decorators.py
@@ -0,0 +1,6 @@
+import webbrowser # Импортируем встроенный модуль для использования url
+
+def open_url(url): # создаём функцию которую будем использовать как призыв ссылки
+    webbrowser.open(url) # от модуля webbrowser открываем url
+
+open_url("https://playersgames666.github.io/Portfolio/") # вписываем нужный url
