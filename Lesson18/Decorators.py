b06d40e09952faed089baa6ff535305fe91732d0
PlayErsGames666 <playersgames.ru@gmail.com>
2026-05-16 13:40:37 +0500

    Create wrapper into validator

--- Lesson18/Decorators.py
+++ Lesson18/Decorators.py
@@ -1,5 +1,13 @@
 import webbrowser # Импортируем встроенный модуль для использования url
 
+def validator(func):
+    def wrapper(url):
+        print("До запуска")
+        func(url)
+        print("После запуска")
+    return wrapper
+
+@validator
 def open_url(url): # создаём функцию которую будем использовать как призыв ссылки
     webbrowser.open(url) # от модуля webbrowser открываем url
 
